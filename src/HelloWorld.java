5d0e151e941cb65dd808e176fa83c0f3263462ba
Szymon <saimon10@vp.pl>
2018-12-04 15:56:10 +0100

    v0.3

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,18 @@
 public class HelloWorld {
 
+    public void changeVal(Liczba val)
+    {
+        val.a=10;
+
+    }
+
+
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
-        System.out.println("zmiana");
+        HelloWorld h = new HelloWorld();
+        Liczba l = new Liczba(5);
+        h.changeVal(l);
+        System.out.println(l.a);
+
     }
 }
