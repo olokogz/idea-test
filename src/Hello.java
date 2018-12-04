920899ac1cc63877e38f2966ef2aaa2a7189ee63
Szymon <saimon10@vp.pl>
2018-12-04 15:21:08 +0100

    nowa klasa

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+
+    public Hello()
+    {
+        System.out.println("Hello World");
+    }
+
+    public void sayHello()
+    {
+        System.out.println("No czesc");
+    }
+}
