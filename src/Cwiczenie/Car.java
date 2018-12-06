b173ab95e232d10966defdc80ed508823cd77433
Szymon <saimon10@vp.pl>
2018-12-06 12:57:01 +0100

    06122018

---
+++ src/Cwiczenie/Car.java
@@ -0,0 +1,9 @@
+package Cwiczenie;
+
+public class Car extends Vehicle{
+
+    @Override
+    public void start() {
+        System.out.println("Car");
+    }
+}
--- src/Traveloffice/TravelOffice.java
+++ src/Traveloffice/TravelOffice.java
@@ -2,16 +2,75 @@ package Traveloffice;
 
 public class TravelOffice {
 
+    private Customer[] customers = new Customer[2];
+    private int customerCount=0;
     public static void main(String[] args) {
+        // utworz biuro podrozy
+        TravelOffice to = new TravelOffice();
 
-        Address address = new Address("Bazylijska","95-100", "Zgierz");
-        Trip trip = new Trip(new Date(2018,12,05),
-                new Date(2018,12,06),"Warszwa");
-        Customer customer = new Customer("Szymon");
+        // budujemy wycieczke
+        Trip trip = new Trip(new Date(2017, 8, 1), new Date(2017, 8, 15), "Egipt");
 
+        // budujemy pierwszego klienta
+        Customer customer = new Customer("Jan Kowalski");
+        customer.setAddress(new Address("Marszalkowska 10", "00-876", "Warszawa"));
         customer.setTrip(trip);
-        customer.setAddress(address);
-        System.out.println(customer.getiInfo());
+        to.addCustomer(customer); // dodajemy go do biura
+
+        customer = new Customer("Jozef Nowak");
+        customer.setAddress(new Address("Al. Jerozolimskie 120", "00-345", "Warszawa"));
+        customer.setTrip(trip);
+        to.addCustomer(customer); // dodajemy go do biura
+
+        customer = new Customer("Jaroslaw Lis");
+        customer.setAddress(new Address("Swietokrzyska 14", "00-666", "Warszawa"));
+        customer.setTrip(trip);
+        to.addCustomer(customer); // dodajemy go do biura
+
+        System.out.println(to.getInfo());
+
 
     }
-}
+
+    private void addCustomer(Customer customer)
+    {
+
+        if (customerCount == customers.length) {
+            Customer temp[] = new Customer[customers.length + 2];
+            System.arraycopy(customers, 0, temp, 0, customers.length);
+            customers = temp;
+        }
+        customers[customerCount++] = customer;
+
+    }
+
+
+
+    private int getCustomerCount()
+    {
+        return customerCount;
+    }
+
+    private String getInfo()
+    {
+        /*StringBuilder sb = new StringBuilder();
+       for(int i=0;i<customerCout;i++)
+       {
+
+               sb.append(tab[i].getiInfo());
+               sb.append("\n");
+
+       }
+        return sb.toString();*/
+
+        String report = "";
+        for (int i = 0; i < customerCount; i++) {
+            report += customers[i].getiInfo() + "\n";
+        }
+        return report;
+
+        }
+    }
+
+
+
